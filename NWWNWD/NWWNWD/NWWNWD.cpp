2832aea3948ca843b76cd24504da97530933d32a
RoksanaB139 <rbrodawczuk139@technikumkreatywne.pl>
2022-04-01 10:10:36 +0200

    NWD I NWW ALE BEZ NWW

---
+++ NWWNWD/NWWNWD/NWWNWD.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cout << "Podaj a" << endl;
+	cin >> a;
+	cout << "Podaj b" << endl;
+	cin >> b;
+
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+
+	}
+	cout <<"NWD " << a;
+
+
+}
