5f696a2b8edbcb7532da454e76f707c53fa441c0
RoksanaB139 <rbrodawczuk139@technikumkreatywne.pl>
2022-04-06 10:39:43 +0200

    skonczone

--- NWWNWD/NWWNWD/NWWNWD.cpp
+++ NWWNWD/NWWNWD/NWWNWD.cpp
@@ -6,12 +6,14 @@ int main()
 {
 	int a;
 	int b;
+	int i;
+	int n;
 
 	cout << "Podaj a" << endl;
 	cin >> a;
 	cout << "Podaj b" << endl;
 	cin >> b;
-
+	i = a * b;
 	while (a != b) {
 		if (a < b) {
 			b = b - a;
@@ -19,9 +21,12 @@ int main()
 		else {
 			a = a - b;
 		}
+		n = i / a;
+
 
 	}
-	cout <<"NWD " << a;
+	cout << "NWD " << a << endl;
+	cout << "NWW " << n;
 
 
 }
