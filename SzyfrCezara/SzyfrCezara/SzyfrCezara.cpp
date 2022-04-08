5f451e55701cadf7d7e748f6702172c580e089cf
RoksanaB139 <rbrodawczuk139@technikumkreatywne.pl>
2022-04-08 10:08:40 +0200

    Szyfr Cezarka

---
+++ SzyfrCezara/SzyfrCezara/SzyfrCezara.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char tab[1000];
+	cout << "Podaj wyraz skladajacy sie z malych liter: " << endl;
+	cin >> tab;
+	
+	int klucz;
+	cout << "Podaj klucz z przedzialu [-26..26]: " << endl;
+	if(!klucz >= -26 and klucz <= 26) {
+
+	}
+	if (klucz >= 0) {
+		for(i < dl) {
+			if tab + kluc <= z{
+				tab += klucz;
+			}
+		}
+	}
+}
