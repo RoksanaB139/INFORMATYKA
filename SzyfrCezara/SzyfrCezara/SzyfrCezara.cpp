736a513376f2c73f10558cccf67e6a6499f111a6
RoksanaB139 <rbrodawczuk139@technikumkreatywne.pl>
2022-04-22 09:30:15 +0200

    Skonczony szyfr cezara

--- SzyfrCezara/SzyfrCezara/SzyfrCezara.cpp
+++ SzyfrCezara/SzyfrCezara/SzyfrCezara.cpp
@@ -3,50 +3,50 @@ using namespace std;
 
 void funkcja(char tab[], int klucz) {
 
+	int dl = strlen(tab);
 
+	if (!(klucz >= -26 and klucz <= 26)) {
+		return;
+	}
 
+	if (klucz >= 0) {
+		for (int i = 0; i < dl; i++) {
+			if (tab[i] + klucz <= 'z') {
+				tab[i] += klucz;
+			}
+			else{
+				tab[i] = tab[i] + klucz - 26;
+			}
+		}
+
+	}
+		
+	else {
+		for (int i = 0; i < dl; i++) {
+			if (tab[i] + klucz >= 'a') {
+				tab[i] += klucz;
+			}
+			else{
+				tab[i] = tab[i] + klucz + 26;
+			}
+		}
+	}
+}
+int main() {
 	char tab[1000];
+
 	cout << "Podaj wyraz skladajacy sie z malych liter: " << endl;
 	cin >> tab;
 
 	int klucz;
 	cout << "Podaj klucz z przedzialu [-26..26]: " << endl;
 	cin >> klucz;
-	int dl = strlen(tab);
-	if (!(klucz >= -26 and klucz <= 26)) {
-		return;
-	}
-	if (klucz >= 0) {
-		for (int i = 0; i < dl; i++) {
-			if (tab[i] + klucz <= z) {
-				tab[i] += klucz;
-			}
-			else if (tab = tab + klucz - 26) {
 
-			}
-		}
-	}
-	//EDYTOWAĆ ELSA!!!
-	/*k6:   inaczej,
-	k7:    Dopóki i < dl,
-    wykonuj krok k8
-	k8:    Jeśli tab + klucz >= a,
-    to tab += klucz,
-    inaczej tab ← tab + klucz + 26,*/
-	else {
-		for (int i = 0; i < dl; i++) {
-			if (tab[i] + klucz <= z) {
-				tab[i] += klucz;
-			}
-			else if (tab = tab + klucz - 26) {
+	funkcja(tab, klucz);
 
-			}
-		}
-	}
-}
+	cout << "Po zaszyfrowaniu: " << tab << endl;
 
-int main()
-{
-	int a = funkcja(tab, klucz);
-	cout << "aa: " << a << endl;
+	funkcja(tab, -klucz);
+
+	cout << "Po rozszyfrowaniu: " << tab << endl;
 }
