e5cf73da166e48f823ed75521856d1becec49857
RoksanaB139 <rbrodawczuk139@technikumkreatywne.pl>
2022-04-22 10:04:19 +0200

    Skonczony ciąg Fibonacciego

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void funkcja(int n);
+
+int main()
+{
+	int n;
+	cout << "Ile wyrazów ciągu wypisać: " << endl;
+	cin >> n;
+	funkcja(n);
+
+}
+void funkcja(int n) {
+	int f0;
+	int f1;
+	int f2;
+	int f;
+
+	f0 = 0;
+	f1 = 1;
+
+	for (int i=0; i <= n; i++) {
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+		cout << f << endl;
+	}
+}
